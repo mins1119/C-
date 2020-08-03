40ea90357c5cb7bc49ca709d961792dcca34347a
mins1119 <elce724@gmail.com>
2020-08-03 17:41:39 +0900

    Interface_Property

---
+++ day9/test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace test1
+{
+    class NameCard
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        {
+            get; set;
+        }
+        public string Name
+        {
+            get; set;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameCard MyCard = new NameCard();
+            MyCard.Name = "Alice";
+            MyCard.Age = 15;
+
+            Console.WriteLine($"이름 : {MyCard.Name}");
+            Console.WriteLine($"나이 : {MyCard.Age}");
+        }
+    }
+}
