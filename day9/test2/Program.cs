25a0b7e405b0430683a80776f52a3a37de7a61e7
mins1119 <elce724@gmail.com>
2020-08-03 18:00:22 +0900

    Interface_Property

---
+++ day9/test2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nameCard = new { Name = "Alice", Age = 17 };
+            Console.WriteLine("이름 : {0}, 나이 : {1}",nameCard.Name, nameCard.Age);
+
+            var Complex = new {Real = 3, Imaginary = -12 };
+            Console.WriteLine("Real : {0}, Imaginary : {1}", Complex.Real, Complex.Imaginary);
+        }
+    }
+}
