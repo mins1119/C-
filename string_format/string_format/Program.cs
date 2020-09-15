b8f5d1f3ebe9ef198fa2800f76b6ab4d2d9201f3
mins1119 <elce724@gmail.com>
2020-09-15 16:53:17 +0900

    c#

---
+++ string_format/string_format/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new FileStream("tabledata2.txt", FileMode.Open));
+            StreamWriter sw = new StreamWriter(new FileStream("table2.txt", FileMode.Create));
+
+            while (sr.EndOfStream == false)
+            {
+                string txt = sr.ReadLine();
+                
+                if(txt.Contains("g_sTableName"))
+                {
+                    Console.WriteLine(txt);
+                    string name = txt.Substring(txt.IndexOf("=") + 1);
+                    sw.WriteLine("{"+ name + ",");
+                    sw.WriteLine("new List<DBFieldInfo>");
+                    sw.WriteLine("{");
+                }
+                if (txt.Contains("Dbary"))
+                {
+                    if (txt[10] == '0') {
+                        sw.Write("new DBFieldInfo(" + txt.Substring(txt.IndexOf("=")+2) + ",");
+                    }
+                    if (txt[10] == '1')
+                    {
+                        sw.WriteLine(txt.Substring(txt.IndexOf("=")+1) + "),");
+                    }
+                }
+
+                if (txt.Contains("Fs_CheckDB Dbary"))
+                {
+                    sw.WriteLine("}");
+                    sw.WriteLine("},");
+                }
+            }
+            sr.Close();
+
+            sw.Close();
+            
+        }
+    }
+}
---
+++ string_format/string_format2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_format2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(new FileStream("tabledata.txt", FileMode.Open));
+            StreamWriter sw = new StreamWriter(new FileStream("table2.txt", FileMode.Create));
+
+            while (sr.EndOfStream == false)
+            {
+                string txt = sr.ReadLine();
+
+                if (txt.Contains("g_sTableName"))
+                {
+                    string name = txt.Substring(txt.IndexOf("=") + 1);
+                    sw.WriteLine("{" + name + ",");
+                    sw.WriteLine("new List<DBFieldInfo>");
+                    sw.WriteLine("{");
+                }
+                try
+                {
+                    if (txt.Contains("Dbary"))
+                    {
+                        if (txt[10] == '0')
+                        {
+                            Console.WriteLine(txt.IndexOf(":"));
+                            Console.WriteLine(txt.IndexOf("="));
+                            int a = txt.IndexOf(":") - txt.IndexOf("=");
+                            sw.Write("new DBFieldInfo(" + txt.Substring(txt.IndexOf("=") + 2, a-2) + ",");
+
+                            sw.WriteLine(txt.Substring(txt.LastIndexOf("=") + 1) + "),");
+                        }
+                    }
+                }
+                catch (Exception e) { }
+                if (txt.Contains("Fs_CheckDB Dbary"))
+                {
+                    sw.WriteLine("}");
+                    sw.WriteLine("},");
+                }
+            }
+            sr.Close();
+
+            sw.Close();
+
+        }
+    }
+}
